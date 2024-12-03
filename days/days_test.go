e935edfdfa0f37b4619a14f1bfcee393debb1944
Max <elenquentaro@gmail.com>
2024-12-03 09:18:14 +0300

    test: improve and unify testing

--- days/days_test.go
+++ days/days_test.go
@@ -5,48 +5,57 @@ import (
 	"testing"
 )
 
-func TestDay1(t *testing.T) {
-	testInput := `3   4
+type testCase struct {
+	input   string
+	expect1 string
+	expect2 string
+}
+
+var testCases = map[uint8]testCase{
+	1: batchTestCase(`3   4
   4   3
   2   5
   1   3
   3   9
-  3   3`
+  3   3`,
+		"11", "31"),
 
-	reader := strings.NewReader(testInput)
-	s := day1Solution{}
-	err := s.ReadData(reader)
-	asserrt(t, err)
-
-	answer1, err := s.SolvePt1()
-	asserrt(t, err)
-	assert(t, answer1 == "11", "%v is incorrect part1 test answer!", answer1)
-
-	answer2, err := s.SolvePt2()
-	asserrt(t, err)
-	assert(t, answer2 == "31", "%v is incorrect part2 test answer", answer2)
-}
-
-func TestDay2(t *testing.T) {
-	testInput := `7 6 4 2 1
+	2: batchTestCase(`7 6 4 2 1
   1 2 7 8 9
   9 7 6 2 1
   1 3 2 4 5
   8 6 4 4 1
-  1 3 6 7 9`
+  1 3 6 7 9`, "2", "4"),
 
-	reader := strings.NewReader(testInput)
-	s := day2Solution{}
-	err := s.ReadData(reader)
-	asserrt(t, err)
+}
 
-	answer1, err := s.SolvePt1()
-	asserrt(t, err)
-	assert(t, answer1 == "2", "%v is incorrect part1 test answer!", answer1)
+func batchTestCase(input, expect1, expect2 string) testCase {
+	return testCase{
+		input:   input,
+		expect1: expect1,
+		expect2: expect2,
+	}
+}
 
-	answer2, err := s.SolvePt2()
-	asserrt(t, err)
-	assert(t, answer2 == "4", "%v is incorrect part2 test answer", answer2)
+func TestDays(t *testing.T) {
+	for d, tc := range testCases {
+		sol, ok := DaySolutions[d]
+		if !ok {
+			t.Logf("there's registered day %v but no solution registered to check", d)
+			continue
+		}
+		reader := strings.NewReader(tc.input)
+		err := sol.ReadData(reader)
+		asserrt(t, err)
+
+		answer1, err := sol.SolvePt1()
+		asserrt(t, err)
+		assert(t, answer1 == tc.expect1, "%v is incorrect part1 test answer!", answer1)
+
+		answer2, err := sol.SolvePt2()
+		asserrt(t, err)
+		assert(t, answer2 == tc.expect2, "%v is incorrect part1 test answer!", answer2)
+	}
 }
 
 func assert(t *testing.T, condition bool, failMsg string, args ...any) {
