b504f3d57f1f3bf07cf62b207b82a2ed9de457f0
Max <elenquentaro@gmail.com>
2024-12-08 10:42:58 +0300

    refactor: move posInt to separate file

--- days/day_06.go
+++ days/day_06.go
@@ -110,31 +110,11 @@ type field struct {
 	height, width int
 }
 
-type posInt struct {
-	x, y int
-}
-
 var dirNames = map[posInt]string{
 	{1, 0}:  "right",
 	{0, 1}:  "down",
 	{-1, 0}: "left",
 	{0, -1}: "up",
-}
-
-func (p posInt) add(other posInt) posInt {
-	return posInt{x: p.x + other.x, y: p.y + other.y}
-}
-
-func (p posInt) rotateRight() posInt {
-	// 1,0 -> 0,1 -> -1,0 -> 0,-1 -> 1,0
-	p.x, p.y = -p.y, p.x
-	// not pointer, so original struct is unchanged
-	return p
-}
-
-// 0,0 is top-left; for more flexible logic it may be rect struct as area
-func (p posInt) within(width, height int) bool {
-	return p.x >= 0 && p.y >= 0 && p.x < width && p.y < height
 }
 
 type actionKind uint8
