cbc72f587e73cfb5cf349d55791d7f0a4a970256
Max <elenquentaro@gmail.com>
2024-12-08 10:50:44 +0300

    feat(util): extend position api

--- days/pos_int.go
+++ days/pos_int.go
@@ -8,6 +8,14 @@ func (p posInt) add(other posInt) posInt {
 	return posInt{x: p.x + other.x, y: p.y + other.y}
 }
 
+func (p posInt) sub(other posInt) posInt {
+	return p.add(other.neg())
+}
+
+func (p posInt) neg() posInt {
+	return posInt{-p.x, -p.y}
+}
+
 func (p posInt) rotateRight() posInt {
 	// 1,0 -> 0,1 -> -1,0 -> 0,-1 -> 1,0
 	p.x, p.y = -p.y, p.x
