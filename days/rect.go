5adc89ce4283e04256ca7a6baa659d6e3a86f5f1
Max <elenquentaro@gmail.com>
2024-12-12 12:07:58 +0300

    feat(utils): commonize byte field scan logic

--- days/rect.go
+++ days/rect.go
@@ -1,7 +1,44 @@
 package days
 
+import "fmt"
+
 type rect struct {
 	topLeft, size posInt
+}
+
+type byteField struct {
+	data [][]byte
+	rect rect
+}
+
+func scanField(reader ioReader) (field byteField, err error) {
+	sc := ProcessReader(reader)
+	defer func() { err = RefineError(err) }()
+
+	var line []byte
+	for err == nil {
+		line, _, err = sc.ReadLine()
+		if err != nil {
+			break
+		}
+
+		// consistency assurance
+		if field.rect.size.x == 0 {
+			field.rect.size.x = len(line)
+		} else if len(line) != field.rect.size.x {
+			err = fmt.Errorf("inconsistent line lens: %v but expected %v",
+				len(line), field.rect.size.x)
+			return
+		}
+
+		row := make([]byte, len(line))
+		for i, b := range line {
+			row[i] = b
+		}
+		field.data = append(field.data, row)
+		field.rect.size.y++
+	}
+	return
 }
 
 // exclude upper bound
