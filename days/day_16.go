7aa263a4a1c0f180a8e3a2766b05f7115d50172d
Max <elenquentaro@gmail.com>
2024-12-16 16:43:36 +0300

    feat(solution): add day 16 part 1 with test case

--- days/day_16.go
+++ days/day_16.go
@@ -54,39 +54,40 @@ func (s *day16Solution) ReadData(reader ioReader) (err error) {
 }
 
 func (s *day16Solution) SolvePt1() (answer string, err error) {
-	startDir := posInt{1, 0} // right
-	startNode := movePositionKeepPrev{
-		parent: s,
-		pos:    s.start,
-		prev:   s.start.sub(startDir),
+	startNode := d16MoveNode{parent: s, posInt: s.start}
+	targetNodeH := startNode
+	targetNodeH.posInt = s.finish
+	targetNodeV := targetNodeH
+	targetNodeV.vertical = true
+	possibleTargets := [2]d16MoveNode{
+		targetNodeH, targetNodeV,
 	}
-	for _, n := range s.finish.neighbors() {
-		if s.getValueAt(n) == s.wall {
-			continue
-		}
-		finishNode := movePositionKeepPrev{
-			parent: s,
-			pos:    s.finish,
-			prev:   n,
-		}
-		path, dist, found := astar.Path(&startNode, &finishNode)
+	for _, finishNode := range possibleTargets {
+		path, dist, found := astar.Path(startNode, finishNode)
 		if !found {
 			plf("not found path %v -> %v", startNode, finishNode)
 			continue
 		}
-		m := make(map[posInt]byte, len(path))
+		m := make(map[posInt]rune, len(path))
 		for _, p := range path {
-			mpkp, ok := p.(*movePositionKeepPrev)
+			mpkp, ok := p.(d16MoveNode)
 			if !ok {
 				plf("UNEXPECTED POINTER VALUE %v", p)
 				continue
 			}
-			m[mpkp.pos] = mpkp.dirToken()
+			r := []rune(mpkp.String())[0]
+			if t, has := m[mpkp.posInt]; has {
+				if t != r {
+					m[mpkp.posInt] = '+'
+				}
+			} else {
+				m[mpkp.posInt] = r
+			}
 		}
 		plf("found path with dist %v:", dist)
 		printRectFunc(s.rect, func(p posInt) rune {
 			if t, ok := m[p]; ok {
-				return rune(t)
+				return t
 			}
 			return rune(s.getValueAt(p))
 		})
@@ -106,6 +107,131 @@ func (s *day16Solution) posToken(p posInt) byte {
 		return 'E'
 	}
 	return s.getValueAt(p)
+}
+
+type d16MoveNode struct {
+	parent *day16Solution
+	posInt
+	vertical bool
+}
+
+func (this d16MoveNode) String() string {
+	sign := "-"
+	if this.vertical {
+		sign = "|"
+	}
+	return fmt.Sprintf("%v {%v}", sign, this.posInt)
+}
+
+func (this d16MoveNode) rotate() d16MoveNode {
+	this.vertical = !this.vertical
+	return this
+}
+
+func (this d16MoveNode) translate(toTopLeft bool) d16MoveNode {
+	direction := dirRight
+	if this.vertical {
+		direction = dirUp
+	}
+	if toTopLeft {
+		direction = direction.neg()
+	}
+	this.posInt = this.add(direction)
+	return this
+}
+
+func (this d16MoveNode) isReachable() bool {
+	p := this.parent
+	return p != nil &&
+		p.rect.contains(this.posInt) &&
+		p.getValueAt(this.posInt) == p.empty
+}
+
+func (this d16MoveNode) PathNeighbors() []astar.Pather {
+	parent := this.parent
+	if parent == nil {
+		// should've be an error but so what
+		return nil
+	}
+
+	count := 1
+	t1 := this.translate(true)
+	t1Valid := t1.isReachable()
+	if t1Valid {
+		count++
+	}
+	t2 := this.translate(false)
+	t2Valid := t2.isReachable()
+	if t2Valid {
+		count++
+	}
+	neighbors := make([]astar.Pather, count)
+	idx := 0
+	neighbors[idx] = this.rotate()
+	idx++
+	if t1Valid {
+		neighbors[idx] = t1
+		idx++
+	}
+	if t2Valid {
+		neighbors[idx] = t2
+	}
+
+	plf("check neighbors for %v: %v", this, neighbors)
+	// DEBUG STUFF
+	printRectFunc(parent.rect, func(p posInt) rune {
+		switch p {
+		case this.posInt:
+			return '+'
+		case this.translate(false).posInt:
+			return []rune(this.translate(false).String())[0]
+		case this.translate(true).posInt:
+			return []rune(this.translate(true).String())[0]
+		default:
+			return rune(parent.posToken(p))
+		}
+	})
+
+	return neighbors
+}
+
+func (this d16MoveNode) PathNeighborCost(to astar.Pather) float64 {
+	const (
+		rotateCost = 1_000
+	)
+	other, ok := to.(d16MoveNode)
+	if !ok {
+		return rotateCost * 1_000_000_000
+	}
+	cost := this.manhDist(other.posInt)
+	if this.vertical != other.vertical {
+		cost += rotateCost
+	}
+	plf("cost from %v to %v is %v", this, other, cost)
+	printRectFunc(this.parent.rect, func(p posInt) rune {
+		switch p {
+		case this.posInt:
+			if this.eq(other.posInt) {
+				return '+'
+			}
+			return []rune(this.String())[0]
+		case other.posInt:
+			return []rune(other.String())[0]
+		default:
+			return rune(this.parent.posToken(p))
+		}
+	})
+	return float64(cost)
+}
+
+func (this d16MoveNode) PathEstimatedCost(to astar.Pather) float64 {
+	// use manhattan distance
+	if other, ok := to.(d16MoveNode); ok {
+		dist := this.manhDist(other.posInt)
+		plf("manh dist from %v to %v is %v", this, other, dist)
+		return float64(dist)
+	}
+	return 1_000_000_000
 }
 
 type movePositionKeepPrev struct {
